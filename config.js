acacb462b109bdcdf11cb75cbb009bc02f980c3b
ManzzyGacor <manzz9293@gmail.com>
2026-04-26 23:03:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   OWNER_IDS: ['7533630775'],
   CHANNEL_USERNAME: '@manzzyidnokos',
   CHANNEL_USERNAME2: '@infomanzzy',
-  GROUP_USERNAME: '@rainepublic',
+  GROUP_USERNAME: '@publicmanji',
   CHANNEL_ID: '-1003891063653',
   CHANNEL_RQ: '-1003891063653',
   BOT_LIMIT: '@ManjiLimitBot',
